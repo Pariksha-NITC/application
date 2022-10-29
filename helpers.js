3ff93f5f718ef7ba92882d2a9867debd75435fd5
atheshwarans <atheshwarans@gmail.com>
2022-10-29 16:27:13 +0530

    minor changes in helpers

--- helpers.js
+++ helpers.js
@@ -2,16 +2,14 @@ const dotenv = require('dotenv')
 dotenv.config();
 const { Client } = require('pg');
 const client = new Client({"connectionString": process.env.CONNECTION_STRING});
-client.connect();
-const executeQuery = (query,logMessage) => {
-	client.query(query, (err, res) => {
-															if (err) {
-																console.log(err);
-																return;
-															}
-															console.log(logMessage);
-															client.end();
-														}
-						);
+
+async function executeQuery(query,logMessage)
+{
+	client.connect();
+	var resp;
+	resp = await client.query(query);
+	console.log(logMessage);
+	client.end();
+	return resp.rows;
 };
 module.exports={executeQuery};
